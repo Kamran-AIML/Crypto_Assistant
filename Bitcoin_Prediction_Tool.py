c9084537efdc734062cc2c01bc462298924a6cd2
Kamran <kamran.aiml.engineer@gmail.com>
2025-08-13 12:35:58 +0530

    Update Bitcoin_Prediction_Tool.py

--- Bitcoin_Prediction_Tool.py
+++ Bitcoin_Prediction_Tool.py
@@ -1,4 +1,41 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+
+import csv
+import logging
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+import time
+import os
+import requests
+from io import StringIO
+
+from deep_translator import GoogleTranslator
+from datetime import datetime
+
+# from selenium.webdriver.firefox.service import Service
+# from selenium.webdriver.firefox.options import Options
+
+from tensorflow.keras.models import load_model
+import joblib
+from datetime import datetime, date, time, timedelta
+
+
+
+
+
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import os
